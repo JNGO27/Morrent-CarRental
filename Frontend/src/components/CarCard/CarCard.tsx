bed74cb8b455dd76f65a16f8ef97e3c3c0cbc41d
sethu <sethumadh@gmail.com>
2022-11-25 13:59:59 +1000

    add favourite functionality using local storage

--- Frontend/src/components/CarCard/CarCard.tsx
+++ Frontend/src/components/CarCard/CarCard.tsx
@@ -1,77 +1,57 @@
 import { FC, useContext, useEffect, useState } from "react"
 
-import { Article, CarCardWrapper, CardRow1, CardRow2, CardRow3, CardRow4, CardSpesification, CardSpesificationDiv, CardTag, CardTitle, Icon, PricePerDay, PricePerDaySmall, RentNowButton } from "./styles"
+import {
+  Article,
+  CarCardWrapper,
+  CardRow1,
+  CardRow2,
+  CardRow3,
+  CardRow4,
+  CardSpesification,
+  CardSpesificationDiv,
+  CardTag,
+  CardTitle,
+  Icon,
+  PricePerDay,
+  PricePerDaySmall,
+  RentNowButton,
+} from "./styles"
 
 import { CarsContext } from "../../contexts/CarsContext"
-import { useModalContext } from "../../contexts/ModalContext"
 import { UserContextObj } from "../../contexts/UserContext"
 
-import carImg from "/src/assets/cars/car.png"
 import { FavoriteRed, GasIcon, Users, Wheel, Favorite } from "../../assets/icon"
 import { Link } from "react-router-dom"
 import { CarType } from "../../contexts/CarsContext"
+import { CarFavouriteContext } from "../../contexts/CarFavouriteContext"
 
-type lSType = {
-  [gId: number]: {
-    [carId:number]:boolean
-  }[]
-}
-
+type CurrentValueType = {
+  carId: number
+  value: boolean
+  gId: number
+}[]
 type CarCardType = {
   car: CarType
 }
 
 const CarCard = ({ car }: CarCardType) => {
-  const [favourite, setFavourite] = useState<boolean>(false)
+  const [toggle, setToggle] = useState<boolean>(false)
   const context = useContext(CarsContext)
-  const { googleId } = useContext(UserContextObj)
-  const gId = parseInt(googleId)
+  const carFavouriteContext = useContext(CarFavouriteContext)
   const { addToFavourite } = context
-  const [userValue, setUserValue] = useState(false)
+  const { handleFavourite, userFavourite } = carFavouriteContext
   const [userId, setUserId] = useState(0)
-  const [carId,setcarId]=useState(car._id)
-  // console.log(userId, userValue, favourite, googleId, gId)
-  // console.log(userId==gId)
-  useEffect(() => {
-    if (gId) {
-      console.log("ran")
-      let lS: lSType = JSON.parse(localStorage.getItem("isFavourite") as string)
-      if (Object.keys(lS).includes(googleId)) {
-        setUserId(gId)
-        setUserValue(false)
-        // console.log(lS, ":LS")
-      } else {
-        setUserId(gId)
-        setUserValue(false)
-        let lS: lSType = JSON.parse(
-          localStorage.getItem("isFavourite") as string
-        )
-        localStorage.setItem(
-          "isFavourite",
-          JSON.stringify({ ...lS, [gId]:{...[carId],[carId]:userValue} })
-        )
-      }
-    } else {
-      setUserId(0)
-      setUserValue(false)
-    }
-  }, [gId])
+  const [userValue, setUserValue] = useState(false)
 
   useEffect(() => {
-    setUserId(userId)
-    setUserValue(favourite)
-    let lS: lSType = JSON.parse(localStorage.getItem("isFavourite") as string)
-    localStorage.setItem(
-      "isFavourite",
-      JSON.stringify({ ...lS,[gId]:{...[carId],[carId]:userValue}})
+    console.log("immediate")
+    let cV: CurrentValueType = userFavourite.filter(
+      (item) => item.carId == car._id
     )
-    // console.log(lS, ":LS")
-  }, [favourite])
-  const handleFavourite = (id: number) => {
-    setFavourite((prevState) => !prevState)
-    addToFavourite(id)
-  }
-  // console.log(googleId)
+    console.log(cV[0], ":currentValue")
+    setUserId(cV[0]?.gId)
+    setUserValue(cV[0]?.value)
+  }, [userFavourite, toggle])
 
   const features = [
     {
@@ -95,8 +75,12 @@ const CarCard = ({ car }: CarCardType) => {
             {car.car_title} <CardTag>{car.car_body_type}</CardTag>
           </CardTitle>
           <Icon
-            src={gId ? (userValue ? FavoriteRed : Favorite) : Favorite}
-            onClick={() => handleFavourite(car._id)}
+            src={userId ? (userValue ? FavoriteRed : Favorite) : Favorite}
+            onClick={() => {
+              handleFavourite(car._id)
+              setToggle(!toggle)
+              addToFavourite(car._id)
+            }}
           />
         </CardRow1>
         <CardRow2>
